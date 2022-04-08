c8aca8dbe744412001138c1b97e54ea27beaa4b8
Amged <hemaisne@yahoo.com>
2022-04-08 23:55:17 +0200

    Initial Commit

---
+++ HAX0R News/HAX0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAX0R_NewsApp.swift
+//  HAX0R News
+//
+//  Created by Amged on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HAX0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
